b41dc2bf00d269c7e95647d7eaf4d86c36022c09
joshuakilaha <jokilaha191@gmail.com>
2019-05-18 20:53:02 +0300

    get request

--- 6 handlebars/routes/index.js
+++ 6 handlebars/routes/index.js
@@ -3,16 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', condition: false });
-});
-
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/users/detail', function(req, res, next) {
-  res.send('detail');
+  res.render('index', { title: 'Express', condition: true, anyArray: [1,2,3] });
 });
 
 
